a9f6f64053394159fcf18847ee51e96ff677757d
sharan.pal <sharan.pal@amebasoftware.com>
2015-11-04 15:07:34 +0530

    new add

---
+++ cameraPro/build/generated/source/buildConfig/release/org/appsroid/fxpro/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.appsroid.fxpro;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "org.appsroid.fxpro";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "";
+}
